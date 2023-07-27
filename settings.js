d5be5944d048b72c8c49b0d9d88a036b589f1c9b
Kataneki <gavintaylor.ai@gmail.com>
2023-07-27 13:52:59 -0500

    Fixed the close button not working on settings menu, added an extra input and some css for the settings menu content.

--- settings.js
+++ settings.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     const settingsButton = document.getElementById("settings-button");
     const settingsBar = document.querySelector(".settings-bar");
-    
-    settingsButton.addEventListener("click", function () {
-      settingsBar.classList.toggle("show-settings");
-    });
-  });
+    const closeSettingsButton = document.getElementById("close-settings-button");
 
-  
+    settingsButton.addEventListener("click", function () {
+        settingsBar.classList.toggle("show-settings");
+    });
+
+    closeSettingsButton.addEventListener("click", function () {
+        settingsBar.classList.remove("show-settings");
+    });
+
+});
