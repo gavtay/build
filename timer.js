7b47eeb53320a3165a3286b25551b97e978a624c
Tay <99550200+gavtay@users.noreply.github.com>
2023-07-31 18:14:12 -0500

    Update timer.js
    Fixed the JS errors, got the timer working to decrement when pressing start.

--- timer.js
+++ timer.js
@@ -1,4 +1,4 @@
-// Changing the start / stop button status whcih tracks if timer is
+// Changing the start / stop button status which tracks if timer is
 // running or not
 document.addEventListener('DOMContentLoaded', function() {
     const startStopStatus = document.querySelector('#start-stop-button');
@@ -20,6 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
             // if the timer is not running and the START button is
             // pressed then change text content to STOP since timer
             // was started
+            console.log('Get ready')
             intervalId = setInterval(updateTimer, 1000);
             startStopStatus.textContent = 'STOP';
         } 
@@ -33,9 +34,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // timer logic here to decrement current time
     function updateTimer() {
-        let hours = parseInt(timerDisplay.hrDisplay.textContent);
-        let minutes = parseInt(timerDisplay.minDisplay.textContent);
-        let seconds = parseInt(timerDisplay.secDisplay.textContent);
+        console.log('In updateTimer function')
+        const hrDisplayValue = document.getElementById('hr-display');
+        const minDisplayValue = document.getElementById('min-display');
+        const secDisplayValue = document.getElementById('sec-display');
+
+        let hours = parseInt(hrDisplayValue.textContent);
+        let minutes = parseInt(minDisplayValue.textContent);
+        let seconds = parseInt(secDisplayValue.textContent);
 
         if (hours === 0 && minutes === 0 && seconds === 0) {
             // If timer is at 0, change pause button to start, set
@@ -44,29 +50,33 @@ document.addEventListener('DOMContentLoaded', function() {
             startStopStatus.textContent = 'START';
             isRunning = false;
             alert('Timer has ended!');
+            console.log('Checked if timer is at 0');
         } 
         else { 
             // else decrement timer
             if (seconds > 0) {
                 seconds--;
-            } else {
-                if (minutes > 0) {
-                    minutes--;
-                    seconds = 59;
-                } else {
-                    if (hours > 0) {
-                        hours--;
-                        minutes = 59;
-                        seconds = 59;
-                    }
-                }
             } 
-                
+            else if (minutes > 0) {
+                minutes--;
+                seconds = 59;
+            } 
+            else if (hours > 0) {
+                hours--;
+                minutes = 59;
+                seconds = 59;
+            }
         }
 
-        hrDisplay.textContent = hours.toString().padStart(2, '0');
-        minDisplay.textContent = minutes.toString().padStart(2, '0');
-        secDisplay.textContent = seconds.toString().padStart(2, '0');
+        // format integer values of time to strings
+        const formattedHours = hours.toString().padStart(2, '0');
+        const formattedMinutes = minutes.toString().padStart(2, '0');
+        const formattedSeconds = seconds.toString().padStart(2, '0');
+
+        // decrement timer
+        hrDisplayValue.textContent = formattedHours;
+        minDisplayValue.textContent = formattedMinutes;
+        secDisplayValue.textContent = formattedSeconds;
     }
 
 });
